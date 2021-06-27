eefaeb04123bbecfb1222ed7ae1479d630a0d3bc
JfmGijsbers <j.f.m.gijsbers@student.tue.nl>
2021-06-27 14:08:18 +0200

    fix

--- classes/User.py
+++ classes/User.py
@@ -10,7 +10,7 @@ class User:
         self.MAC = getmacbyip(self.IP)
         if (self.MAC) == "ff:ff:ff:ff:ff:ff":
             self.MAC = ""
-            print(f"FATAL ERROR: could not find MAC for IP ${self.IP}")
+            print(f'FATAL ERROR: could not find MAC for IP {self.IP}')
 
     def set_mac(self, MAC):
         self.MAC = MAC
