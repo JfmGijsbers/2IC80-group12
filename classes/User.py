0efc7f33e8a69b22bf730786bcaf0a6cc62e08cb
JfmGijsbers <j.f.m.gijsbers@student.tue.nl>
2021-06-27 13:59:27 +0200

    added print

--- classes/User.py
+++ classes/User.py
@@ -7,6 +7,7 @@ class User:
 
     def get_mac(self):
         result = sr(ARP(op=ARP.who_has, psrc=self.IP, pdst='192.168.56.102'))
+        print(result)
         self.MAC = result[0][ARP].hwsrc
         print(self.MAC)
         # arp_req_frame = ARP(pdst = self.IP)
