74e5654aad566b6c6b63c23c17db1d9cd07a7b99
JfmGijsbers <j.f.m.gijsbers@student.tue.nl>
2021-06-27 12:15:18 +0200

    scapy import

--- classes/User.py
+++ classes/User.py
@@ -1,12 +1,14 @@
+from scapy.all import *
+
 class User:
     def __init__(self, IP):
         self.IP = IP
 
     def get_mac(self):
-        arp_req_frame = scapy.ARP(pdst = self.ip)
-        broadcast_ether_frame = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
+        arp_req_frame = ARP(pdst = self.ip)
+        broadcast_ether_frame = Ether(dst = "ff:ff:ff:ff:ff:ff")
         broadcast_ether_arp_req_frame = broadcast_ether_frame / arp_req_frame
-        answered_list = scapy.srp(broadcast_ether_arp_req_frame, timeout = 1, verbose = False)[0]
+        answered_list = srp(broadcast_ether_arp_req_frame, timeout = 1, verbose = False)[0]
         self.MAC = answered_list[0][1].hwsrc
         return answered_list[0][1].hwsrc
 
