0d9a824b8470477bdd48cb4d0bfb54419d0b11e7
JfmGijsbers <j.f.m.gijsbers@student.tue.nl>
2021-06-27 13:55:33 +0200

    get_mac fix

--- classes/User.py
+++ classes/User.py
@@ -6,12 +6,15 @@ class User:
         self.MAC = MAC
 
     def get_mac(self):
-        arp_req_frame = ARP(pdst = self.IP)
-        broadcast_ether_frame = Ether(dst = "ff:ff:ff:ff:ff:ff")
-        broadcast_ether_arp_req_frame = broadcast_ether_frame / arp_req_frame
-        answered_list = srp(broadcast_ether_arp_req_frame, timeout = 1, verbose = False)[0]
-        self.MAC = answered_list[0][1].hwsrc
-        return answered_list[0][1].hwsrc
+        result = sr(ARP(op=ARP.who_has, psrc='X.X.X.X', pdst='Y.Y.Y.Y'))
+        self.MAC = result[0][ARP].hwsrc
+        print(self.MAC)
+        # arp_req_frame = ARP(pdst = self.IP)
+        # broadcast_ether_frame = Ether(dst = "ff:ff:ff:ff:ff:ff")
+        # broadcast_ether_arp_req_frame = broadcast_ether_frame / arp_req_frame
+        # answered_list = srp(broadcast_ether_arp_req_frame, timeout = 1, verbose = False)[0]
+        # self.MAC = answered_list[0][1].hwsrc
+        # return answered_list[0][1].hwsrc
 
     def set_mac(self, MAC):
         self.MAC = MAC
