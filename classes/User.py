8cebe8fce1a9fa87caae5fcd1518d0aa7f69885c
JfmGijsbers <j.f.m.gijsbers@student.tue.nl>
2021-06-27 14:33:23 +0200

    Re-implemented mitm

--- classes/User.py
+++ classes/User.py
@@ -7,11 +7,9 @@ class User:
         self.MAC = MAC
 
     def get_mac(self):
-        print(getmacbyip(self.IP))
         self.MAC = getmacbyip(self.IP)
         if (self.MAC) == "ff:ff:ff:ff:ff:ff":
             self.MAC = gma()
-            print(gma())
 
     def set_mac(self, MAC):
         self.MAC = MAC
