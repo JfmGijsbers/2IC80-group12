47208ced922dfa227b35f7196f56553a1cc6c61c
jeroengi <jfmgijsbers@gmail.com>
2021-06-29 14:16:04 +0200

    ga

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ echo Please enter the IP of the gateway
 read gateway_ip
 echo 'Please enter your own IP (optional, will be calculated if empty)'
 read local_ip
-echo Please enter the interface (optional, default is enp0s3)
+echo 'Please enter the interface (optional, default is enp0s3)'
 read interface
 echo $target_ip $gateway_ip $local_ip $interface
