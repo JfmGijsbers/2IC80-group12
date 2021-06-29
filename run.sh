739a72416df765200dd6b8f16943831e39dd53f8
jeroengi <jfmgijsbers@gmail.com>
2021-06-29 14:14:34 +0200

    Added sh run script

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo Please enter the IP of the victim
+read target_ip
+echo Please enter the IP of the gateway
+read gateway_ip
+echo 'Please enter your own IP (optional, will be calculated if empty)'
+read local_ip
+echo Please enter the interface (optional, default is enp0s3)
+read interface
+echo $target_ip $gateway_ip $local_ip $interface
