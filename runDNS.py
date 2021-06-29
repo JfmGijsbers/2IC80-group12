be47cf6ece774d4ef2c81100c2880291b7477eb2
theeleking <kevindirksen12@gmail.com>
2021-06-29 15:42:46 +0200

    ip update

--- runDNS.py
+++ runDNS.py
@@ -3,9 +3,9 @@ from DNS import DNS
 if __name__ == '__main__':
     #redirects to httpforever
     dns_hosts = {
-        b"www.google.com.": "104.21.51.146",
-        b"google.com.": "104.21.51.146",
-        b"facebook.com.": "104.21.51.146"
+        b"www.google.com.": "10.0.2.4",
+        b"google.com.": "10.0.2.4",
+        b"facebook.com.": "10.0.2.4"
     }
 
     dns = DNS(dns_hosts, 0)
