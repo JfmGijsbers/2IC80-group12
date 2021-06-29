f8664966075d8473c48e51355431c13d4570551d
jeroengi <jfmgijsbers@gmail.com>
2021-06-29 14:22:42 +0200

    gwg

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ echo 'Please enter your own IP (optional, will be calculated if empty)'
 read local_ip
 echo 'Please enter the interface (optional, default is enp0s3)'
 read interface
-
+echo $local_ip
 if ( "$local_ip"); then
   echo "$local_ip"
 else
